b17eb1e0900e4f07fe1697f53888ce0b6123262d
El Messoudi Zakaria <zakaria.elmessoudi@sofrecom.com>
2020-10-22 16:35:13 +0200

    style: update repo item component

--- src/containers/TrendingRepos/components/TrendingRepoItem.js
+++ src/containers/TrendingRepos/components/TrendingRepoItem.js
@@ -6,14 +6,35 @@
  * Modified By: El Messoudi Zakaria (you@you.you>)
  * -----
  */
-import React from 'react'
+import React from "react";
 
-const TrendingRepoItem = (repo)=> {
-    return (
-       <li>
-           <label>{repo.repo.name}</label>
-       </li>
-    )
-}
+const TrendingRepoItem = (repo) => {
+  return (
+    <li className="list-group-item">
+      <div className="item-card">
+        <div className="row">
+          <div className="col-md-2">
+            <img
+              src={repo.repo.avatar}
+              alt="avatar"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+          <div className="col-md-10">
+            <div>
+              <h3>{repo.repo.name}</h3>
+              <p>{repo.repo.description}</p>
+              <div>
+                <label>Stars {repo.repo.stars}</label>
+                <label>Issues {repo.repo.issues}</label>
+                <label>Submitted {repo.repo.lastSubmittedTime}</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default TrendingRepoItem;
